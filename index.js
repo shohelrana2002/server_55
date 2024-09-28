423b8f4d58f60e76644ab71aa25dbb29990b1dda
shohelrana2002 <rana2021tang@gmail.com>
2024-09-28 12:23:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const cors = require("cors");
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//  uzAkUKKAkgJEP0oq
+//  module_55
+
+// middle were
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://module_55:uzAkUKKAkgJEP0oq@cluster0.ykv18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("usersDB").collection("Users");
+    // const userCollection = database.collection("Users");
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log("NEw user", user);
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment.jjj You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My Phone server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
